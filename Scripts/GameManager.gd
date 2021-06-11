96538b57d3fd9768c14680bbd0fd4893b4357f25
Joshua Dyer <dyerjo@mail.gvsu.edu>
2021-06-11 15:54:36 -0500

    game manager to serve as a singleton

---
+++ Scripts/GameManager.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var total_players = 0
+var current_player_count = 0
+var EndGame = load('res://Levels/EndGame.tscn')
+var time = 0
+
+#func _ready():
+#	get_tree().paused = true
+
+#func _process(delta: float):
+#	if ENDGAMECONDITION, then 
+#		get_tree().paused = true
+#		var end_game = EndGame.instance()
+#		get_tree().get_root().add_child(end_game)
