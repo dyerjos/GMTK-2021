eb17ab348bbf1c01729847cb77420e1975a566dd
Joshua Dyer <dyerjo@mail.gvsu.edu>
2021-06-13 09:41:32 -0500

    clean code

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,21 +11,11 @@ export (int) var JUMP_FORCE = 300
 export (int) var MAX_SLOPE_ANGLE = 46
 export (String) var greenColor = "e5ff00" # player 2 color
 
-#enum {
-#	MOVE,
-#	PUSH_PULL
-#}
-
-#var state = MOVE
 var motion = Vector2.ZERO
 var snap_vector = Vector2.ZERO
 var direction = 1 # left = -1, right = 1
-#var is_rotating = false
 var just_jumped = false
 var is_magnetized = true
-#var is_connected = false
-#var carried_by = null
-#var is_carrying = false
 var other_players_active_side = null
 var players_active_side = null
 var repel_or_attract = null
@@ -41,22 +31,15 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	just_jumped = false
-	
-#	match state:
-#		MOVE:
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
-#	update_snap_vector() # TODO: delete this?
 	jump_check()
 	apply_gravity(delta)
-#	update_animations(input_vector) # TODO: delete this?
 	move()
 	apply_polarity()
 	apply_magnetism()
-		
-#		PUSH_PULL:
-#			move()
+
 
 func get_input_vector():
 	var input_vector = Vector2.ZERO
@@ -73,10 +56,6 @@ func apply_horizontal_force(input_vector, delta):
 func apply_friction(input_vector):
 	if input_vector.x == 0 and is_on_floor():
 		motion.x = lerp(motion.x, 0, FRICTION)
-
-func update_snap_vector():
-	if is_on_floor():
-		snap_vector = Vector2.DOWN
 
 func jump_check():
 	if is_on_floor() or jumpTimer.time_left > 0:
@@ -95,23 +74,6 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
-		
-#func update_animations(input_vector):
-#	print('updating animations')
-#	var facing = sign(get_local_mouse_position().x)
-#	if facing != 0:
-#		sprite.scale.x = facing
-#
-#	if input_vector.x != 0:
-#		spriteAnimator.play("Run")
-#		spriteAnimator.playback_speed = input_vector.x * sprite.scale.x
-#	else:
-#		spriteAnimator.playback_speed = 1
-#		spriteAnimator.play("Idle")
-#
-#	if not is_on_floor():
-#		spriteAnimator.play("Jump")
-#	pass
 
 func move():
 	var was_in_air = not is_on_floor()
@@ -169,11 +131,6 @@ func apply_magnetism():
 			influence_direction = player_pos.direction_to(other_pos)
 		elif repel_or_attract == "repel":
 			influence_direction = -1 * player_pos.direction_to(other_pos)
-#			if repelTimer.time_left == 0:
-#				repel_or_attract = null
-#				other_players_active_side = null
-#				players_active_side = null
-#			repelTimer.start()
 		if distance > 10 and distance < 200:
 			add_central_force(influence_direction * GRAVITY_STRENGTH)
 			move_and_slide(currentTotalForce)
@@ -183,7 +140,6 @@ func apply_magnetism():
 			other_players_active_side = null
 			players_active_side = null
 			
-		
 		
 func add_central_force(toAdd : Vector2):
 	currentTotalForce += toAdd
